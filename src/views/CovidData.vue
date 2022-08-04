ab3cbeb0fc48e4529fee00c7b4cfc3b563314445
Ghulam Jaffar <herojaf12@gmail.com>
2022-08-04 21:59:01 +0500

    complete compo api

--- src/views/CovidData.vue
+++ src/views/CovidData.vue
@@ -16,6 +16,10 @@
 import DataTitleVue from '@/components/DataTitle.vue';
 import DataCardsVue from '@/components/DataCards.vue';
 import CountrySelectVue from '@/components/CountrySelect.vue';
+import { onMounted, ref } from '@vue/composition-api';
+import loadGif from '../assets/loader.gif';
+import axios from 'axios';
+
 
 export default {
   name: 'CovidData',
@@ -23,40 +27,50 @@ export default {
     DataTitleVue,
     DataCardsVue,
     CountrySelectVue
-  }, data() {
-    return {
-      loading: true,
-      title: 'Global',
-      date: '',
-      stats: {},
-      countries: [],
-      loader: require('../assets/loader.gif'),
-    }
-  }, methods: {
-    async fetchData() {
-      const res = await this.$axios.get('https://api.covid19api.com/summary');
+  }, setup() {
+    const loading = ref(true)
+    const title = ref('Global')
+    const date = ref('')
+    const stats = ref({})
+    const countries = ref([])
+    const loader = ref(loadGif)
+    onMounted(async () => {
+      const data = await fetchData()
+      date.value = data.data.Date
+      stats.value = data.data.Global
+      countries.value = data.data.Countries
+      loading.value = false
+    })
+    const fetchData = async () => {
+      const res = await axios.get('https://api.covid19api.com/summary');
       return res
-    },
-    getCountry(country) {
-      this.title = country.Country;
-      this.stats = country;
-    },
-    async clearCountryData() {
-      this.loading = true
-      const data = await this.fetchData()
-      this.date = data.data.Date
-      this.title = 'Global'
-      this.stats = data.data.Global
-      this.countries = data.data.Countries
-      this.loading = false
     }
-  },
-  async created() {
-    const data = await this.fetchData()
-    this.date = data.data.Date
-    this.stats = data.data.Global
-    this.countries = data.data.Countries
-    this.loading = false
+
+    const getCountry = (country) => {
+      title.value = country.Country;
+      stats.value = country;
+    }
+
+    const clearCountryData = async () => {
+      loading.value = true
+      const data = await fetchData()
+      date.value = data.data.Date
+      title.value = 'Global'
+      stats.value = data.data.Global
+      countries.value = data.data.Countries
+      loading.value = false
+    }
+    return {
+      loading,
+      title,
+      date,
+      stats,
+      countries,
+      loader,
+      fetchData,
+      getCountry,
+      clearCountryData
+    }
   }
 }
 </script>
